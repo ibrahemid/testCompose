7cce012a289f247bd77cda65c48a507df16d5320
Ibrahem Mahyob <ibrahemid@gmail.com>
2023-01-27 01:01:10 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(Libs.androidGradlePlugin)
+        classpath(Libs.Kotlin.gradlePlugin)
+    }
+}
+
+plugins {
+    id("com.diffplug.spotless") version ("6.2.0")
+    id("com.github.ben-manes.versions") version("0.39.0")
+}
+allprojects {
+
+    apply {
+        plugin("com.diffplug.spotless")
+    }
+    repositories {
+        google()
+        mavenCentral()
+    }
+    spotless {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("$buildDir/**/*.kt")
+            targetExclude("bin/**/*.kt")
+            targetExclude("buildSrc/**/*.kt")
+            ktlint(Libs.ktLintVersion).userData(mapOf("android" to "true"))
+        }
+    }
+}
